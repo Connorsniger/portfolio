b82157e74e4189ba6d5884927570f53513a238c4
Connorsniger <33581827+Connorsniger@users.noreply.github.com>
2018-01-20 11:40:30 -0500

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,13 @@
+import random 
+choice=(random.randint(1,3))
+
+user=input("what do you want to do,1 rock,2 paper, or 3 scissors? ")
+
+
+if choice==1 and user=="rock":
+    print("you tied")
+if choice==1 and user=="paper":
+    print("you win")
+if choice==1 and user=="scissors":
+    print ("you lose")
+if choice==
